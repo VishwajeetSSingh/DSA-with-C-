5b2a6341a0d95520855d78c286debfe4186de249
Vishwajeet Singh <116628921+VishwajeetSSingh@users.noreply.github.com>
2024-03-05 20:55:58 +0530

    Add files via upload

---
+++ SwapTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//Swapping of two numbers
+int main(){
+	int x,y,temp;
+	cout<<"Enter the value of x and y\n";
+	cin>>x>>y;
+	cout<<"Before Swapping\n x="<<x<<"\n y="<<y<<endl;
+	temp = x;
+	x = y;
+	y = temp;
+	cout<<"After Swapping\n x="<<x<<"\n y="<<y<<endl;
+	return 0;
+}
